c85bccba005039be51c7723bd9627bcbf45c4cc2
liang <xiaoleliang045@gmail.com>
2021-08-29 23:52:41 +0900

    1. reconstruct this project into maven parent-son project 2. add eureka project as son module

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.course.couse;
+package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CouseApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CouseApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
