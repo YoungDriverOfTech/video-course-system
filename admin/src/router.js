ac25ff8c96bb77dbffa632ac95c341a281fb9b49
liang <xiaoleliang045@gmail.com>
2021-09-02 23:16:42 +0900

    1. login component jump to admin component

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './view/login.vue'
+import Admin from './view/admin.vue'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
         {
             path: '/login',
             component: Login
+        },
+        {
+            path: '/admin',
+            component: Admin
         }
     ]
 })
