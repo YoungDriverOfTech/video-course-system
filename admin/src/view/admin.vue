d5aa9ba1c5013910d83379177c076bcbe48ac913
liang <xiaoleliang045@gmail.com>
2021-09-03 22:13:35 +0900

    1. use mounted method complete component initial

--- admin/src/view/admin.vue
+++ admin/src/view/admin.vue
@@ -1156,9 +1156,16 @@
   </div>
 </template>
 <script>
-$('body').removeClass('login-layout light-login');
-$('body').attr('class', 'no-skin');
+
 export default {
-    
+    name: 'admin',
+    mounted: function() {
+      $('body').removeClass('login-layout light-login');
+      $('body').attr('class', 'no-skin');
+      console.log('admin');
+    },
+    methods: {
+
+    }
 }
 </script>
