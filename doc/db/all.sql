6acdc9e81dc8ec39b2a33312a9dadbaf2c4fc1fa
liang <xiaoleliang045@gmail.com>
2021-09-06 21:11:18 +0900

    1. add Chapter table sql 2. generate dao source code

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,13 @@
+drop table if exists `chapter`;
+create table `chapter` (
+  `id` char(8) not null comment 'Id',
+  `course_id` char(8) comment 'Course Id',
+  `name` varchar(50) comment 'Name',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='Chapter';
+
+
+-----for test======
 drop table if exists `test`;
 create table `test` (
   `id` char(8) not null default '' comment 'id',
