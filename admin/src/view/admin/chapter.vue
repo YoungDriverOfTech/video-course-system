fde2121eb438d978897204a15e8c79929a47f226
liang <xiaoleliang045@gmail.com>
2021-09-07 00:01:39 +0900

    1. complete frontend access to backend 2. resolve cross origin problem by CorsConfig

--- admin/src/view/admin/chapter.vue
+++ admin/src/view/admin/chapter.vue
@@ -1287,9 +1287,16 @@ export default {
     name: 'chapter',
     mounted: function() {
         // this.$parent.activeSidebar('business-chapter-sidebar');
+        let _this = this;
+        _this.list();
     },
     methods: {
-
+        list() {
+            let _this = this;
+            _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response) => {
+                console.log(response);
+            })
+        }
     }
 };
 </script>
