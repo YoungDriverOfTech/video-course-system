09be1aae2a73d2ffa0031d2d0b1a90af24d912e9
liang <xiaoleliang045@gmail.com>
2021-09-09 22:20:51 +0900

    1. complete update chapter

--- admin/src/view/admin/chapter.vue
+++ admin/src/view/admin/chapter.vue
@@ -31,85 +31,13 @@
           <td>{{ chapter.courseId }}</td>
           <td>
             <div class="hidden-sm hidden-xs btn-group">
-              <button class="btn btn-xs btn-success">
-                <i class="ace-icon fa fa-check bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-info">
+              <button v-on:click="edit(chapter)" class="btn btn-xs btn-info">
                 <i class="ace-icon fa fa-pencil bigger-120"></i>
               </button>
 
               <button class="btn btn-xs btn-danger">
                 <i class="ace-icon fa fa-trash-o bigger-120"></i>
               </button>
-
-              <button class="btn btn-xs btn-warning">
-                <i class="ace-icon fa fa-flag bigger-120"></i>
-              </button>
-            </div>
-
-            <div class="hidden-md hidden-lg">
-              <div class="inline pos-rel">
-                <button
-                  class="btn btn-minier btn-primary dropdown-toggle"
-                  data-toggle="dropdown"
-                  data-position="auto"
-                >
-                  <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                </button>
-
-                <ul
-                  class="
-                    dropdown-menu
-                    dropdown-only-icon
-                    dropdown-yellow
-                    dropdown-menu-right
-                    dropdown-caret
-                    dropdown-close
-                  "
-                >
-                  <li>
-                    <a
-                      href="#"
-                      class="tooltip-info"
-                      data-rel="tooltip"
-                      title="View"
-                    >
-                      <span class="blue">
-                        <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                      </span>
-                    </a>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      class="tooltip-success"
-                      data-rel="tooltip"
-                      title="Edit"
-                    >
-                      <span class="green">
-                        <i
-                          class="ace-icon fa fa-pencil-square-o bigger-120"
-                        ></i>
-                      </span>
-                    </a>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      class="tooltip-error"
-                      data-rel="tooltip"
-                      title="Delete"
-                    >
-                      <span class="red">
-                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                      </span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
             </div>
           </td>
         </tr>
@@ -169,9 +97,17 @@ export default {
   },
   methods: {
     add() {
-      // let _this = this;
+      let _this = this;
+      _this.chapter = {};
       $('#add-chapter-modal-form').modal('show');
-      console.log('success');
+    },
+
+    edit(chapter) {
+      let _this = this;
+      // using parameter chapter directly will afffect table displayed in page, thus make a deep copy for parameter chapter
+      // $.extend(target, source);  this deep copy method is provided by jquery 
+      _this.chapter = $.extend({}, chapter);
+      $('#add-chapter-modal-form').modal('show');
     },
 
     list(page) {
