f6337c5a0308e4e01ceeb624a64557ec3dc039fc
liang <xiaoleliang045@gmail.com>
2021-09-11 00:38:14 +0900

    1. optimize Log output(front end)

--- admin/src/main.js
+++ admin/src/main.js
@@ -6,6 +6,19 @@ import axios from 'axios'
 Vue.config.productionTip = false
 Vue.prototype.$ajax = axios;
 
+/**
+ * axios interceptor
+ */
+axios.interceptors.request.use(function (config) {
+  console.log("请求：", config);
+  return config;
+}, error => { console.log(error) });
+axios.interceptors.response.use(function (response) {
+  console.log("返回结果：", response);
+  return response;
+}, error => { console.log(error) });
+
+
 new Vue({
   router,
   render: h => h(App),
