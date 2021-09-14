316722f5cff9bf29b22523dc2bcec4e3a437b90f
liang <xiaoleliang045@gmail.com>
2021-09-14 19:10:06 +0900

    1. update generator

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -3,7 +3,11 @@ package com.course.generator.server;
 import com.course.generator.util.DbUtil;
 import com.course.generator.util.Field;
 import com.course.generator.util.FreemarkerUtil;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.util.*;
 
 public class ServerGenerator {
@@ -12,17 +16,36 @@ public class ServerGenerator {
     static String toDtoPath = "server\\src\\main\\java\\com\\course\\server\\dto\\";
     static String toServicePath = "server\\src\\main\\java\\com\\course\\server\\service\\";
     static String toControllerPath = "business\\src\\main\\java\\com\\course\\business\\controller\\admin\\";
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
 
     public static void main(String[] args) throws Exception {
-        String domainUpperCase = "Section";
-        String domainLowerCase = "section";
-        String tableNameCn = "Section";
         String module = MODULE;
-        List<Field> fieldList = DbUtil.getColumnByTableName(domainLowerCase);
+
+        // only generate first table node in xml file
+        File file = new File(generatorConfigPath);
+        SAXReader reader = new SAXReader();
+        // read xml file into Document obejct
+        Document doc = reader.read(file);
+        // get root element
+        Element rootElement = doc.getRootElement();
+        // get context node
+        Element contextElement = rootElement.element("context");
+        // define a Element obj for loop
+        Element tableElement;
+        // get first table node
+        tableElement = contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("Table："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
+
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
         Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
-        map.put("Domain", domainUpperCase);
-        map.put("domain", domainLowerCase);
+        map.put("Domain", Domain);
+        map.put("domain", domain);
         map.put("tableNameCn", tableNameCn);
         map.put("module", module);
         map.put("fieldList", fieldList);
@@ -30,22 +53,21 @@ public class ServerGenerator {
 
         // generate Dto
         FreemarkerUtil.initConfig("dto.ftl");
-        FreemarkerUtil.generator(toDtoPath + domainUpperCase + "Dto.java", map);
+        FreemarkerUtil.generator(toDtoPath + Domain + "Dto.java", map);
 
         // generate service layer
         FreemarkerUtil.initConfig("service.ftl");
-        FreemarkerUtil.generator(toServicePath + domainUpperCase + "Service.java", map);
+        FreemarkerUtil.generator(toServicePath + Domain + "Service.java", map);
 
         // generate controller layer
         FreemarkerUtil.initConfig("controller.ftl");
-        FreemarkerUtil.generator(toControllerPath + domainUpperCase + "Controller.java", map);
+        FreemarkerUtil.generator(toControllerPath + Domain + "Controller.java", map);
 
     }
 
     private static Set<String> getJavaTypes(List<Field> fieldList) {
         Set<String> set = new HashSet<>();
-        for (int i = 0; i < fieldList.size(); i++) {
-            Field field = fieldList.get(i);
+        for (Field field : fieldList) {
             set.add(field.getJavaType());
         }
         return set;
