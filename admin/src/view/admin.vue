7649319821c0f6467d89cab7b0995391a971172f
liang <xiaoleliang045@gmail.com>
2021-09-20 17:36:15 +0900

    1. delete useless code

--- admin/src/view/admin.vue
+++ admin/src/view/admin.vue
@@ -458,32 +458,6 @@
         </div>
       </div><!-- /.main-content -->
 
-      <div class="footer">
-        <div class="footer-inner">
-          <div class="footer-content">
-						<span class="bigger-120">
-							<span class="blue bolder">甲蛙</span>
-							在线视频课程 &copy; 2099-2099
-						</span>
-
-            &nbsp; &nbsp;
-            <span class="action-buttons">
-							<a href="#">
-								<i class="ace-icon fa fa-twitter-square light-blue bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-facebook-square text-primary bigger-150"></i>
-							</a>
-
-							<a href="#">
-								<i class="ace-icon fa fa-rss-square orange bigger-150"></i>
-							</a>
-						</span>
-          </div>
-        </div>
-      </div>
-
       <a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
         <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
       </a>
