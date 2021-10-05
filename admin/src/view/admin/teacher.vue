5ade2220d2c522dee876ab6a83ca45e98e8b6057
liang <xiaoleliang045@gmail.com>
2021-10-05 21:20:04 +0900

    1. complete file component

--- admin/src/view/admin/teacher.vue
+++ admin/src/view/admin/teacher.vue
@@ -99,11 +99,10 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">image</label>
                 <div class="col-sm-10">
-                  <button type="button" v-on:click="selectImage()" class="btn btn-white btn-default btn-round">
-                    <i class="ace-icon fa fa-upload"></i>
-                    Upload Icon
-                  </button>
-                  <input class="hidden" type="file" ref="file" v-on:change="uploadImage()" id="file-upload-input">
+                  <file v-bind:id="'image-upload'"
+                        v-bind:text="'上传头像'"
+                        v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                        v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -150,8 +149,9 @@
 
 <script>
 import Pagination from "../../components/pagination";
+import File from "../../components/file";
 export default {
-  components: { Pagination },
+  components: { Pagination, File },
   name: "business-teacher",
   data: function () {
     return {
@@ -248,42 +248,10 @@ export default {
       });
     },
 
-    uploadImage() {
+    afterUpload(resp) {
       let _this = this;
-      let formData = new window.FormData();
-      let file = _this.$refs.file.files[0];
-
-      // judge file type
-      let suffixs = ['jpg', 'jpeg', 'png'];
-      let fileName = file.name;
-      let suffix = fileName.substring(fileName.lastIndexOf('.') +1, fileName.length).toLowerCase();
-        let validateSuffix = false;
-        for (let i = 0; i < suffixs.length; i++) {
-          if (suffixs[i].toLowerCase() === suffix) {
-            validateSuffix = true;
-            break;
-          }
-        }
-        if (!validateSuffix) {
-          Toast.warning("File type unsupported! Followings are allowed：" + suffixs.join(","));
-          return;
-        }
-
-      // key: 'file',   this name must same with backend's parameter
-      formData.append('file', file);
-      
-      Loading.show();
-      _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then(response => {
-        Loading.hide();
-        let resp = response.data;
-        let image = resp.content;
-        console.log("image address: " + image);
-        _this.teacher.image = image;
-      });
-    },
-
-    selectImage() {
-      $('#file-upload-input').trigger('click');
+      let image = resp.content;
+      _this.teacher.image = image;
     },
   },
 };
