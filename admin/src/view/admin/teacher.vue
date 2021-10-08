03734e252714773a6492acc2f762a074bbfd4f98
liang <xiaoleliang045@gmail.com>
2021-10-08 22:59:27 +0900

    1. save file according to file's application

--- admin/src/view/admin/teacher.vue
+++ admin/src/view/admin/teacher.vue
@@ -102,6 +102,7 @@
                   <file v-bind:id="'image-upload'"
                         v-bind:text="'上传头像'"
                         v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                        v-bind:use="FILE_USE.TEACHER.key"
                         v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
@@ -157,6 +158,7 @@ export default {
     return {
       teachers: [],
       teacher: {},
+      FILE_USE: FILE_USE
     };
   },
   mounted: function () {
