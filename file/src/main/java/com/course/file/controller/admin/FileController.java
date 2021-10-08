1f6d84335667641d99c584b2cd3491456859413b
liang <xiaoleliang045@gmail.com>
2021-10-08 22:39:54 +0900

    1. delete file component unnecessary function

--- file/src/main/java/com/course/file/controller/admin/FileController.java
+++ file/src/main/java/com/course/file/controller/admin/FileController.java
@@ -4,10 +4,12 @@ import com.course.server.dto.FileDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.FileService;
-import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -27,31 +29,6 @@ public class FileController {
         ResponseDto<PageDto<FileDto>> responseDto = new ResponseDto<>();
         fileService.list(pageDto);
         responseDto.setContent(pageDto);
-        return responseDto;
-    }
-
-    @PostMapping("/save")
-    public ResponseDto<FileDto> save(@RequestBody FileDto fileDto) {
-        LOG.info("fileDto: {}", fileDto);
-
-        // check operation
-        ValidatorUtil.require(fileDto.getPath(), "relative path");
-        ValidatorUtil.length(fileDto.getPath(), "relative path", 1, 100);
-        ValidatorUtil.length(fileDto.getName(), "file name", 1, 100);
-        ValidatorUtil.length(fileDto.getSuffix(), "suffix", 1, 10);
-
-        // save operation
-        ResponseDto<FileDto> responseDto = new ResponseDto<>();
-        fileService.save(fileDto);
-        responseDto.setContent(fileDto);
-        return responseDto;
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto<FileDto> delete(@PathVariable String id) {
-        LOG.info("id: {}", id);
-        ResponseDto<FileDto> responseDto = new ResponseDto<>();
-        fileService.delete(id);
         return responseDto;
     }
 }
