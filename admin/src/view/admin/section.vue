258791267805b62ba62030e03c5de5665782c236
liang <xiaoleliang045@gmail.com>
2021-10-10 22:38:01 +0900

    1. get video length

--- admin/src/view/admin/section.vue
+++ admin/src/view/admin/section.vue
@@ -99,7 +99,7 @@
                         v-bind:after-upload="afterUpload"></file>
                   <div v-show="section.video" class="row">
                     <div class="col-md-9">
-                      <video v-bind:src="section.video" controls="controls"></video>
+                      <video v-bind:src="section.video" id="video" controls="controls"></video>
                     </div>
                   </div>
                 </div>
@@ -261,7 +261,14 @@ export default {
       let _this = this;
       let video = resp.content.path;
       _this.section.video = video;
-    }
+      _this.getTime();
+    },
+
+    getTime() {
+      let _this = this;
+      let ele = document.getElementById("video");
+      _this.section.time = parseInt(ele.duration, 10);
+    },
   },
 };
 </script>
