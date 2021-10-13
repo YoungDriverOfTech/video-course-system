ecec6ddb794b9315e51a8b03e99aa9973e419617
liang <xiaoleliang045@gmail.com>
2021-10-13 21:18:40 +0900

    1. merge file

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -8,6 +8,7 @@ import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 @RequestMapping("/admin")
@@ -72,5 +75,43 @@ public class UploadController {
         responseDto.setContent(fileDto);
         return responseDto;
 
+    }
+
+    @GetMapping("/merge")
+    public ResponseDto merge() throws Exception {
+        File newFile = new File(FILE_PATH + "course/111.mp4");
+        FileOutputStream outputStream = new FileOutputStream(newFile, true);
+        FileInputStream fileInputStream = null; // partial file
+        byte[] byt = new byte[10 * 1024 * 1024];
+        int len;
+
+        try {
+            // read first partial file
+            fileInputStream = new FileInputStream(new File(FILE_PATH +  "/course/Bc0SXtFn.blob"));
+            while ((len = fileInputStream.read(byt)) != -1) {
+                outputStream.write(byt, 0, len);
+            }
+
+            // read second partial file
+            fileInputStream = new FileInputStream(new File(FILE_PATH +  "/course/roQbPm2x.blob"));
+            while ((len = fileInputStream.read(byt)) != -1) {
+                outputStream.write(byt, 0, len);
+            }
+        } catch (IOException e) {
+            LOG.error(e.toString());
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+                outputStream.close();
+                LOG.info("close IO stream");
+            } catch (Exception e) {
+                LOG.error(e.toString());
+            }
+        }
+
+        ResponseDto responseDto = new ResponseDto();
+        return responseDto;
     }
 }
