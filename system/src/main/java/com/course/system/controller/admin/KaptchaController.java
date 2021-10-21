9d0eb26f35ea7e04e67ad3b2c41c39ab9afe9288
liang <xiaoleliang045@gmail.com>
2021-10-21 11:45:32 +0900

    1. add kaptcha module

---
+++ system/src/main/java/com/course/system/controller/admin/KaptchaController.java
@@ -0,0 +1,55 @@
+package com.course.system.controller.admin;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+@RestController
+@RequestMapping("/admin/kaptcha")
+public class KaptchaController {
+
+    @Qualifier("getDefaultKaptcha")
+    @Autowired
+    DefaultKaptcha defaultKaptcha;
+
+    public void imageCode(@PathVariable(value = "imageCodeToken") String imageCodeToken,
+                          HttpServletRequest request,
+                          HttpServletResponse response) throws Exception {
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        try {
+            // generate token string
+            String createText = defaultKaptcha.createText();
+
+            // set createText into session for certificating
+            request.getSession().setAttribute(imageCodeToken, createText);
+
+            // generate certification picture through createText
+            BufferedImage challenge = defaultKaptcha.createImage(createText);
+            ImageIO.write(challenge, "jpg", jpegOutputStream);
+        } catch (IllegalArgumentException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // define response output stream as image/jpeg, output image as byte array
+        byte[] captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream = response.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+}
