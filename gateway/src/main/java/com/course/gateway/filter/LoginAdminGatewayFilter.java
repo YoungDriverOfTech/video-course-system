1953807ca3cf5f86dab942115ea99c3055bf2835
liang <xiaoleliang045@gmail.com>
2021-10-21 18:40:17 +0900

    1. gate implement filter

--- gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
+++ gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
@@ -5,20 +5,51 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
 
 @Component
 public class LoginAdminGatewayFilter implements GatewayFilter, Ordered {
     private static final Logger LOG = LoggerFactory.getLogger(LoginAdminGatewayFilter.class);
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (!path.contains("/admin/")) {
+            return chain.filter(exchange);
+        }
+        if (path.contains("/system/admin/user/login")
+                || path.contains("/system/admin/user/logout")
+                || path.contains("/system/admin/kaptcha")) {
+            LOG.info("no need to certificate：{}", path);
+            return chain.filter(exchange);
+        }
         // get token from headers
         String token = exchange.getRequest().getHeaders().getFirst("token");
-        LOG.info("filter work: {}", token);
-        return chain.filter(exchange);
+        LOG.info("token：{}", token);
+        if (token == null || token.isEmpty()) {
+            LOG.info( "token is empty, request is blocked" );
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        Object object = redisTemplate.opsForValue().get(token);
+        if (object == null) {
+            LOG.warn( "token is invalid" );
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        } else {
+            LOG.info("login finished：{}", object);
+            return chain.filter(exchange);
+        }
     }
 
     @Override
